50cb163261c1ae8374645d2a90c454b0785ac420
Jason Hogrefe <hogrefejason@gmail.com>
2015-02-06 22:03:40 -0600

    Fixed Foreign Key data types as Integers.

--- database_setup.rb
+++ database_setup.rb
@@ -1,20 +1,20 @@
 DATABASE.results_as_hash = true
 
 DATABASE.execute("CREATE TABLE IF NOT EXISTS locations 
-                 (id INTEGER PRIMARY KEY,
-                  location_name TEXT)")
+                  (id INTEGER PRIMARY KEY,
+                   location_name TEXT)")
 
 DATABASE.execute("CREATE TABLE IF NOT EXISTS categories 
-                 (id INTEGER PRIMARY KEY,
-                  manufacturer TEXT)")
-                  
+                  (id INTEGER PRIMARY KEY,
+                   manufacturer TEXT)")
+                   
 DATABASE.execute("CREATE TABLE IF NOT EXISTS products 
-                 (id INTEGER PRIMARY KEY,
-                 name TEXT, 
-                 FOREIGN KEY(location_id) REFERENCES locations(id),
-                 FOREIGN KEY(category_id) REFERENCES categories(id),
-                 description TEXT, 
-                 cost INTEGER,
-                 serial TEXT,
-                 quantity INTEGER,
-                 )")
+                  (id INTEGER PRIMARY KEY,
+                  name TEXT, 
+                  FOREIGN KEY(location_id) REFERENCES locations(id) INTEGER,
+                  FOREIGN KEY(category_id) REFERENCES categories(id) INTEGER,
+                  description TEXT, 
+                  cost INTEGER,
+                  serial TEXT,
+                  quantity INTEGER,
+                  )")
